9d1e2edd2f6b94eb9c54534aeb76161df6e623bd
IgorV <viazminiv@gmail.com>
2021-05-13 10:40:31 +0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 `use strict`;
 
+const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+
 gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./source/sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('./css'));
 });
@@ -12,3 +14,10 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: 'public'
+    });
+    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+})
